a4659ba7cb4108f08a0538f73f52f8c1018b59d1
Daniele Bucchi <d.bucchi@domotz.com>
2024-11-21 17:29:22 +0100

    change writeLine in english

--- Scripts/DayExtractor.cs
+++ Scripts/DayExtractor.cs
@@ -30,7 +30,6 @@ class DayExtractor {
             try
             {
                 // Fai la richiesta GET per ottenere il contenuto
-                Console.WriteLine("SONO QUI " + url);
                 var request = new HttpRequestMessage(HttpMethod.Get, url);
                 request.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36");
                  request.Headers.Add("Cookie", $"session={this.cookie}");
